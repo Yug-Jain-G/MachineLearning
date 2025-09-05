640b2d529c7ea7e687bd84093c5082f176562b25
Yug-Jain-G <yugjain592@gmail.com>
2025-09-05 22:00:18 +0530

    Update Data_preprocessing_tools.py

--- Data Preprocessing/Data_preprocessing_tools.py
+++ Data Preprocessing/Data_preprocessing_tools.py
@@ -16,3 +16,22 @@ x[: , 1:3] = imputer.transform(x[:, 1:3])
 
 #Encoding the independent variable
 from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+#Encoding the dependent variable
+from sk.learn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+#Splitting the dataset into the training set and test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_test = train_test_split(x,y, test_size = 0.2 , random_state = 1)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
