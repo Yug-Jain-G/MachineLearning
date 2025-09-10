0f26cd5795aeb3c695af47565e36718b6e3e483f
Yug-Jain-G <yugjain592@gmail.com>
2025-09-10 17:31:10 +0530

    Update simple_linear_regression.py

--- Regression/Simple Linear Regression/simple_linear_regression.py
+++ Regression/Simple Linear Regression/simple_linear_regression.py
@@ -1,1 +1,24 @@
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Salarty_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
+
+#Training the Simple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+#Predicting the Test set results
+y_pred = regressor.predict(x_test)
+
+#
 
